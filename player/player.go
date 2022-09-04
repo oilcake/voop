6b435865c563b76f8c3bdb89cec32b899f150dc8
oilcake <oilpie@gmail.com>
2022-09-04 21:58:23 +0300

    switched case to range in player

--- player/player.go
+++ player/player.go
@@ -19,17 +19,14 @@ func (p *Player) PlayMedia() {
 	var k int
 	// play it in cycle forever
 play:
-	for {
-		select {
-		case <-p.Clock.Trigger:
-			// calculate a playing phase
-			ph := p.Media.Position(p.Transport)
-			fmt.Printf("\rCurrent beat is %.9f and phase is %.9f", (<-p.Transport.Status).Beat, ph)
-			// to retrieve specific frame
-			img := p.Media.Frame(ph)
-			// and display it
-			p.Window.IMShow(img)
-		}
+	for range p.Clock.Trigger {
+		// calculate a playing phase
+		ph := p.Media.Position(p.Transport)
+		fmt.Printf("\rCurrent beat is %.9f and phase is %.9f", (<-p.Transport.Status).Beat, ph)
+		// to retrieve specific frame
+		img := p.Media.Frame(ph)
+		// and display it
+		p.Window.IMShow(img)
 		k = p.Window.WaitKey(19)
 		switch {
 		case k == 27:
