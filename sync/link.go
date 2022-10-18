46f1ecb478961cb3fcba9e37b6909562191459ea
oilcake <oilpie@gmail.com>
2022-10-18 22:33:37 +0300

    comments clean up

--- sync/link.go
+++ sync/link.go
@@ -1,37 +1,15 @@
 package sync
 
-// func NewConnection() (conn net.Conn) {
-//     // open socket
-//     conn, err := net.Dial(protocol, address)
-//     if err != nil {
-//         log.Fatalf("cannot establish connection\nDo you have Carabiner running?\n %v", err)
-//         return nil
-//     }
-//     return
-// }
-
 func NewLink(st chan Status, crbnr Carabiner) {
 	var (
-		// response *string
-		// err      error
 		watch    Status
 		oldTempo float32
 		newTempo float32
 	)
 	watch.Bpm = 0.0
-	// start Ableton Link watcher
-	// conn := NewConnection()
 	go func() {
 		for {
 			oldTempo = watch.Bpm
-			// response, err = Ping(conn, "status")
-			// if err != nil {
-			//     log.Fatal("no response from Carabiner", err)
-			// }
-			// err = Parse(response, &watch)
-			// if err != nil {
-			//     log.Fatal("Parsing error", err)
-			// }
 			watch = crbnr.Provide()
 			newTempo = watch.Bpm
 			if oldTempo != newTempo {
@@ -44,18 +22,3 @@ func NewLink(st chan Status, crbnr Carabiner) {
 	}()
 
 }
-
-// func Parse(message *string, st *Status) error {
-//     err := edn.Unmarshal([]byte(*message), st)
-//     return err
-// }
-
-// func Ping(conn net.Conn, message string) (*string, error) {
-//     fmt.Fprintf(conn, "%s", message)                        // send message
-//     response, err := bufio.NewReader(conn).ReadString('\n') // listen response
-//     if err != nil {
-//         return nil, err
-//     }
-//     response = response[7:]
-//     return &response, nil
-// }
